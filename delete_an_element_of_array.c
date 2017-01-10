cdcd1e76ec9c870c643d93a36e1a2b8b877be267
Cyberboy <cyberboysumanjay@gmail.com>
2017-01-10 15:13:51 +0530

    Updated code
    Now user can insert their values in array!

--- delete_an_element_of_array.c
+++ delete_an_element_of_array.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 void main()
 {
-int i,j, a[5]={2,4,6,7,9},pos,n=5;
-printf("Enter the position of element you want to rm-rf");
+int i,j, a[5],pos,n=5,c;
+printf("Enter 5 elements of array\n");
+{
+  for (c=0;c<5;c++)
+  scanf("%d",&a[c]);
+}
+printf("Enter the position of element you want to rm-rf \n");
 scanf ("%d",&pos);
   for (i=pos-1;i<n-1;i++)
   {
      a[i]=a[i+1];
-
   }
   for (j=0;j<4;j++)
   {
