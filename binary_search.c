1da91e96b5262ca52b347d7c1f07f34d8aabc0e2
Cyberboy <cyberboysumanjay@gmail.com>
2017-01-10 14:47:50 +0530

    Added CSB Lab Assignment
    Linear and Binary searches

---
+++ binary_search.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+   int c, f, l, m, n, search, a[100];
+
+   printf("Enter number of elements\n");
+   scanf("%d",&n);
+
+   printf("Enter %d integers\n", n);
+
+   for (c = 0; c < n; c++)
+      scanf("%d",&a[c]);
+
+   printf("Enter value to search\n");
+   scanf("%d", &search);
+
+   f = 0;
+   l = n - 1;
+   m = (f+l)/2;
+
+   while (f <= l)
+   {
+      if (a[m] < search)
+      f= m+1;
+    else if (a[m] == search)
+          {
+           printf("%d found at %d th postion.\n", search, m+1);
+           break;
+           }
+    else
+    l = m - 1;
+    m = (f+l)/2;
+  }
+      if (f > l)
+      printf("No Such number present  in the list.\n");
+return 0;
+}
