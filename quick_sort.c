38c1dba53d2be12b5384aa64d8be5c42935af17a
Cyberboy <cyberboysumanjay@gmail.com>
2017-05-01 19:28:21 +0530

    Added New QuickSort Program
    Bcoz engineers study a day before the exams!

--- quick_sort.c
+++ quick_sort.c
@@ -1,60 +1,66 @@
-#include<stdio.h>
-void quicksort (int [],int,int);
-int partition (int [],int,int);
-int main ()
+#include <stdio.h>
+
+void quick_sort(int[],int,int);
+int partition(int[],int,int);
+
+int main()
 {
-int a[100],n,i;
-printf("Enter the number of elements in array:\n");
-scanf("%d",&n);
-printf("Enter the elements of the array:\n");
-  for (i=0;i<n;i++)
-  {
-    scanf("%d",&a[n]);
-  }
+    int a[50],n,i;
+    printf("How many elements?");
+    scanf("%d",&n);
+    printf("\nEnter array elements:");
 
-quicksort(a,0,n-1);
-printf("Array after quick sorting is :\n");
-for (i=0;i<n;i++)
-printf("%d\n",a[i]);
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
 
-return 0;
+    quick_sort(a,0,n-1);
+    printf("\nArray after sorting:");
+
+    for(i=0;i<n;i++)
+        printf("%d ",a[i]);
+
+    return 0;
 }
-void quicksort (int a[], int left, int right)
-{
-int j;
-  if (left<right)
-  {
-  j=partition(a,left,right);
-  quicksort(a,left,j-1);
-  quicksort(a,j+1,right);
-  }
-}
-int partition (int a[],int left,int right)
-{
-int x,i,j,temp;
-x=a[left];
-i=left;
-j=right+1;
 
-  do
-  {
-      do
-      i++;
-  while (a[i]<x&&i<=right);
+void quick_sort(int a[],int l,int u)
+{
+    int j;
+    if(l<u)
+    {
+        j=partition(a,l,u);
+        quick_sort(a,l,j-1);
+        quick_sort(a,j+1,u);
+    }
+}
+
+int partition(int a[],int l,int u)
+{
+    int v,i,j,temp;
+    v=a[l];
+    i=l;
+    j=u+1;
+
     do
-     j--;
+    {
+        do
+            i++;
 
+        while(a[i]<v&&i<=u);
 
-  while (x<a[j]);
-  if (i<j)
-  {
-  temp=a[i];
-  a[i]=a[j];
-  a[j]=temp;
-  }
-  }
-  while(i<j);
-  a[left]=a[j];
-  a[j]=x;
-  return (j);
-  }
+        do
+            j--;
+        while(v<a[j]);
+
+        if(i<j)
+        {
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+        }
+    }while(i<j);
+
+    a[l]=a[j];
+    a[j]=v;
+
+    return(j);
+}
