df676809c35f680aeab34cad2379c9e70d292709
Sumanjay <cyberboysumanjay@gmail.com>
2018-05-13 15:40:23 +0530

    Upgraded Insertion Sort Code to C++

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,29 +1,31 @@
-
-#include<stdio.h>
-int main()
+#include<iostream>
+using namespace std;
+int  main()
 {
-int i,j,n,temp,a[100];
+    int size, arr[50], i, j, temp;
 
-  printf("Enter total elements: ");
-  scanf("%d",&n);
-
-  printf("Enter %d elements: ",n);
-  for(i=0;i<n;i++)
-      scanf("%d",&a[i]);
-
-  for(i=1;i<n;i++)
+    cout<<"Enter Array Size : ";
+    cin>>size;
+    cout<<"Enter Array Elements : ";
+    for(i=0; i<size; i++)
     {
-      temp=a[i];
-      j=i-1;
-      while((temp<a[j])&&(j>0))
-      {
-      a[j+1]=a[j];
-      j=j-1;
-      }
-      a[j+1]=temp;
-  }
-
-  printf("Array in ascending order:\n ");
-  for(i=0;i<n;i++)
-  printf(" %d",a[i]);
+        cin>>arr[i];
+    }
+    for(i=1; i<size; i++)
+    {
+        temp=arr[i];
+        j=i-1;
+        while((temp<arr[j]) && (j>=0))
+        {
+            arr[j+1]=arr[j];
+            j=j-1;
+        }
+        arr[j+1]=temp;
+    }
+    cout<<"Sorted Array : \n";
+    for(i=0; i<size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
 }
